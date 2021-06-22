d3b914dc3e3b249816a497862c08daa7a2ffd693
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-06-22 14:39:43 +0530

    Delete Compile.bat

--- Compile.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-title Compile AOs
-
-echo Compiling.
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
-
-move .\bin\Release\netcoreapp3.1\win-x64\publish\AOs.exe .\AOs\AOs.exe
-exit
