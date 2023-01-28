28d6b20926476970fbff52aec016c940b6469c53
Light-Lens <QCoreNest@gmail.com>
2023-01-28 22:44:43 +0530

    Update UPR.py and build

--- UPR.py
+++ UPR.py
@@ -1,28 +1,27 @@
-# IMPORTS
 from colorama import init, Fore, Style
-import argparse
-import requests
+from rich.progress import track
+import argparse, requests, time
 
-# Initialize Command-line arguments.
+# initialize Command-line arguments.
 Parser = argparse.ArgumentParser(description="Check for Latest AOs Updates")
-Parser.add_argument("-v", help="Enter the current version")
-Args = Parser.parse_args()
+Parser.add_argument("-v", help="Current running version of AOs")
+args = Parser.parse_args()
 
-# Initialize Updater.
+# initialize.
 init(autoreset = True)
 
 # Check for Updates
-if not Args.v:
-    print(f"{Fore.RED}{Style.BRIGHT}YOU MUST ENTER A VERSION,")
-    print(f"TYPE {Fore.CYAN}{Style.BRIGHT}'-v [VERSION]'")
+total_size = 1
+for _ in track(range(total_size), description="Checking for Updates...\n"):
+    response = requests.get("https://api.github.com/repos/Light-Lens/AOs/releases/latest", timeout=1, stream=True, verify=True)
+    total_size = int(response.headers.get("Content-Length", 0))
+    time.sleep(0.001)
 
+if args.v >= response.json()["name"]: print("You're up to date.")
 else:
-    response = requests.get("https://api.github.com/repos/Light-Lens/AOs/releases/latest")
-    if response.json()["name"] == Args.v: print("You're up to date.")
-    else:
-        print("Updates available.")
+    print("Updates are available.")
 
-        print(f"Your version: {Fore.BLUE}{Style.BRIGHT}{Args.v}")
-        print(f"Latest version: {Fore.BLUE}{Style.BRIGHT}{response.json()['name']}")
+    print(f"Your version: {Fore.BLUE}{Style.BRIGHT}{args.v}")
+    print(f"Latest version: {Fore.BLUE}{Style.BRIGHT}{response.json()['name']}")
 
-        print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}https://github.com/Light-Lens/AOs/releases")
+    print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}https://github.com/Light-Lens/AOs/releases/tag/v{response.json()['name']}")
--- build.bat
+++ build.bat
@@ -4,13 +4,17 @@ title Starting build
 @REM Check if AOs folder exists, if yes, remove it.
 if EXIST AOs rmdir /s /q AOs 
 
-@REM First compile AOs with Dotnet then safe with gcc (C++)
+@REM Compile project
 dotnet publish -c Release -o ./AOs
 
-echo Compiling safe.cpp
+echo. && echo Compiling safe.cpp
 g++ safe.cpp -o Sysfail/rp/safe.exe
+
+echo. && echo Compiling UPR.py
+pyinstaller -y --clean --onefile --icon=img/UPR.ico --add-data "img;img" -n UPR UPR.py --distpath SoftwareDistribution\UpdatePackages\
 
 @REM Move all necessary folders to the build folder.
 robocopy "Sysfail" "AOs/Sysfail" /e /nfl /ndl /njh /njs /nc /ns /np /is /it
 robocopy "Sysfail/RECOVERY" "AOs" /e /nfl /ndl /njh /njs /nc /ns /np /is /it
-rmdir /s /q bin,obj
+rmdir /s /q bin,obj,build
+del UPR.spec
