db613fba29512aefbacf1b15aca962511e0b0c6a
Light-Lens <QCoreNest@gmail.com>
2023-02-03 21:29:31 +0530

    change 'name' to 'tag_name' in UPR while checking for updates

--- UPR.py
+++ UPR.py
@@ -9,19 +9,21 @@ args = Parser.parse_args()
 
 # initialize.
 init(autoreset = True)
+repo = "Light-Lens/AOs"
 
 # Check for Updates
 total_size = 1
 for _ in track(range(total_size), description="Checking for Updates...\n"):
-    response = requests.get("https://api.github.com/repos/Light-Lens/AOs/releases/latest", timeout=1, stream=True, verify=True)
+    response = requests.get(f"https://api.github.com/repos/{repo}/releases/latest", timeout=1, stream=True, verify=True)
     total_size = int(response.headers.get("Content-Length", 0))
     time.sleep(0.001)
 
-if args.v >= response.json()["name"]: print("You're up to date.")
+tag_name = response.json()["tag_name"]
+if args.v >= tag_name[1:]: print("You're up to date.")
 else:
     print("Updates are available.")
 
     print(f"Your version: {Fore.BLUE}{Style.BRIGHT}{args.v}")
     print(f"Latest version: {Fore.BLUE}{Style.BRIGHT}{response.json()['name']}")
 
-    print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}https://github.com/Light-Lens/AOs/releases/tag/v{response.json()['name']}")
+    print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}https://github.com/{repo}/releases/download/{tag_name}/AOs.zip")
