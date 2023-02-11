059b827b91b92818eb2f24c3e1c2fe6bf0af91ab
Light-Lens <QCoreNest@gmail.com>
2023-02-11 19:50:25 +0530

    Change venv to .venv

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ title Starting build
 
 @REM Activate venv before compiling.
 @REM This is make sure nothing breaks while compiling.
-call venv\Scripts\activate
+call .venv\Scripts\activate
 
 @REM Check if AOs folder exists, if yes, remove it.
 if EXIST AOs rmdir /s /q AOs
