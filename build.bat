ba050e78c9627de459dfe6f5d36b7ac2e97491df
Light-Lens <QCoreNest@gmail.com>
2023-02-13 07:58:15 +0530

    change build system

--- build.bat
+++ build.bat
@@ -11,15 +11,15 @@ if EXIST AOs rmdir /s /q AOs
 @REM Compile project
 dotnet publish -c Release -o ./AOs
 
-echo. && echo Compiling UPR.py
-pyinstaller --onefile --icon=img/UPR.ico UPR.py --distpath Sysfail\RECOVERY\SoftwareDistribution\UpdatePackages
+echo. && echo Compiling C++ scripts
+g++ "Sysfail\rp\safe.cpp" -o Sysfail/rp/safe.exe
 
-echo. && echo Compiling safe.cpp
-g++ safe.cpp -o Sysfail/rp/safe.exe
+echo. && echo Compiling Python scripts
+pyinstaller --onefile --icon=img/UPR.ico "Sysfail\RECOVERY\SoftwareDistribution\UpdatePackages\UPR.py" --distpath Sysfail\RECOVERY\SoftwareDistribution\UpdatePackages
 
 @REM Move all necessary folders to the build folder.
 echo. && echo Finishing build
-robocopy "Sysfail" "AOs/Sysfail" /e /nfl /ndl /njh /njs /nc /ns /np /is /it
-robocopy "Sysfail/RECOVERY" "AOs" /e /nfl /ndl /njh /njs /nc /ns /np /is /it
+robocopy "Sysfail" "AOs/Sysfail" /e /nfl /ndl /njh /njs /nc /ns /np /is /it /xf *.py
+robocopy "Sysfail/RECOVERY" "AOs" /e /nfl /ndl /njh /njs /nc /ns /np /is /it /xf *.py
 rmdir /s /q bin,obj,build
-del UPR.spec
+del *.spec
