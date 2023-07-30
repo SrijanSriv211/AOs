89dfa635743f4fd391336c1d89d039b7939b01b3
Light-Lens <QCoreNest@gmail.com>
2023-07-30 20:09:14 +0530

    $1

--- github.bat
+++ github.bat
@@ -3,11 +3,11 @@
 echo -> Checking status
 git status
 
-echo. & echo -> Adding file(s)
+echo. & echo ~ Adding file(s) & echo.
 git add .
 
-echo. & echo -> Commiting updates
+echo. & echo ~ Commiting updates & echo.
 git commit -m $1
 
-echo. & echo -> Pushing updates
+echo. & echo ~ Pushing updates
 git push origin master
