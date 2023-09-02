c6b9252fa7c9fdb4d82b11ae4c96fee0cde05538
Light-Lens <QCoreNest@gmail.com>
2023-09-02 19:10:00 +0530

    Uncomment all commented code

--- src/AOs/EntryPoint.cs
+++ src/AOs/EntryPoint.cs
@@ -40,85 +40,83 @@ class EntryPoint
 
         var parsed_args = parser.Parse(argv);
 
-        // if (parsed_args.Count() == 0)
-        // {
-        //     Console.WriteLine("Ters");
-        //     Environment.Exit(0);
-        //     string startlist_path = Path.Combine(Obsidian.rootDir, "Files.x72\\root\\StartUp\\.startlist");
-        //     bool isEmpty = Utils.String.IsEmpty(
-        //         FileIO.FileSystem.ReadAllText(startlist_path)
-        //     );
+        if (parsed_args.Count() == 0)
+        {
+            string startlist_path = Path.Combine(Obsidian.rootDir, "Files.x72\\root\\StartUp\\.startlist");
+            bool isEmpty = Utils.String.IsEmpty(
+                FileIO.FileSystem.ReadAllText(startlist_path)
+            );
 
-        //     if (File.Exists(startlist_path) && !isEmpty)
-        //     {
-        //         AOs.Entrypoint(false);
-        //         foreach (string appname in File.ReadLines(startlist_path))
-        //         {
-        //             // break if "." is in place of appname
-        //             // all apps after the dot will be marked as disabled.
-        //             if (appname == ".")
-        //                 break;
+            if (File.Exists(startlist_path) && !isEmpty)
+            {
+                AOs.Entrypoint(false);
+                foreach (string appname in File.ReadLines(startlist_path))
+                {
+                    // break if "." is in place of appname
+                    // all apps after the dot will be marked as disabled.
+                    if (appname == ".")
+                        break;
 
-        //             else if (appname.EndsWith(".aos"))
-        //             {
-        //                 foreach (string current_line in FileIO.FileSystem.ReadAllLines( Path.Combine(Path.GetDirectoryName(startlist_path), appname) ))
-        //                     Run(AOs.TakeInput(current_line));
-        //             }
-        //         }
-        //     }
+                    else if (appname.EndsWith(".aos"))
+                    {
+                        foreach (string current_line in FileIO.FileSystem.ReadAllLines( Path.Combine(Path.GetDirectoryName(startlist_path), appname) ))
+                            Run(AOs.TakeInput(current_line));
+                    }
+                }
+            }
 
-        //     else
-        //     {
-        //         AOs.Entrypoint();
-        //         while (true)
-        //             Run(AOs.TakeInput());
-        //     }
-        // }
+            else
+            {
+                AOs.Entrypoint();
+                while (true)
+                    Run(AOs.TakeInput());
+            }
+        }
 
-        // else
-        // {
-        //     foreach (var arg in parsed_args)
-        //     {
-        //         Console.WriteLine($"[({string.Join(", ", arg.Names)}), {arg.Value}, is_flag: {arg.Is_flag}]");
+        else
+        {
+            foreach (var arg in parsed_args)
+            {
+                Console.WriteLine($"[({string.Join(", ", arg.Names)}), {arg.Value}, is_flag: {arg.Is_flag}]");
 
-        //         if (Argparse.IsAskingForHelp(arg.Names))
-        //             parser.PrintHelp();
+                if (Argparse.IsAskingForHelp(arg.Names))
+                    parser.PrintHelp();
 
-        //         else if (arg.Names.Contains("-c"))
-        //         {
-        //             string out_value = arg.Value;
-        //             if (out_value == null || Utils.String.IsEmpty(out_value))
-        //                 return;
+                else if (arg.Names.Contains("-c"))
+                {
+                    string out_value = arg.Value;
+                    if (out_value == null || Utils.String.IsEmpty(out_value))
+                        return;
 
-        //             AOs.Entrypoint(false);
-        //             Run(AOs.TakeInput(out_value));
-        //         }
+                    AOs.Entrypoint(false);
+                    Run(AOs.TakeInput(out_value));
+                }
 
-        //         else
-        //         {
-        //             AOs.Entrypoint(false);
-        //             foreach (string filename in arg.Names)
-        //             {
-        //                 if (!filename.EndsWith(".aos"))
-        //                 {
-        //                     new Error($"{filename}: File format not recognized.");
-        //                     return;
-        //                 }
+                else
+                {
+                    AOs.Entrypoint(false);
+                    foreach (string filename in arg.Names)
+                    {
+                        if (!filename.EndsWith(".aos"))
+                        {
+                            new Error($"{filename}: File format not recognized.");
+                            return;
+                        }
 
-        //                 else if (!File.Exists(filename))
-        //                 {
-        //                     new Error($"{filename}: No such file or directory.");
-        //                     return;
-        //                 }
+                        else if (!File.Exists(filename))
+                        {
+                            new Error($"{filename}: No such file or directory.");
+                            return;
+                        }
 
-        //                 else
-        //                 {
-        //                     foreach (string current_line in FileIO.FileSystem.ReadAllLines(filename))
-        //                         Run(AOs.TakeInput(current_line));
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+                        else
+                        {
+                            foreach (string current_line in FileIO.FileSystem.ReadAllLines(filename))
+                                Run(AOs.TakeInput(current_line));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
