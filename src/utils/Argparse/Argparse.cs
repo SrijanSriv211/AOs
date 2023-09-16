555aa0592c2a419546aa6b05e37eaf03b3b08e6f
Light-Lens <QCoreNest@gmail.com>
2023-09-16 22:58:01 +0530

    rework Argparse. Make it similar to Parser but slightly different

--- src/utils/Argparse/Argparse.cs
+++ src/utils/Argparse/Argparse.cs
@@ -5,11 +5,14 @@ partial class Argparse
 
     private readonly List<Argument> arguments = new();
     private readonly Dictionary<string[], string> help_list = new();
+    private readonly Action<string> error_func;
 
-    public Argparse(string name, string description)
+    public Argparse(string name, string description, Action<string> error_func=null)
     {
         this.name = name;
         this.desc = description;
+
+        this.error_func = error_func;
     }
 
     public void Add(string[] cmd_names, string help_message, string[] default_values=null, bool is_flag=true, bool required=false, int min_args_length=0, int max_args_length=0, Delegate method=null)
@@ -21,68 +24,168 @@ partial class Argparse
         arguments.Add(new Argument(cmd_names, help_message, default_values, is_flag, required, min_args_length, max_args_length, method));
     }
 
-    public List<ParsedArgument> Parse(string[] args, Action<string> error_func=null)
+    public List<ParsedArgument> Parse(string[] args)
     {
         List<ParsedArgument> parsed_args = new();
         string[] arg_flags = { "--", "-", "/" };
 
         for (int i = 0; i < args.Length; i++)
         {
-            string arg = args[i];
-            if (arg_flags.Any(prefix => arg.StartsWith(prefix)))
+            string lowercase_arg = args[i].ToLower();
+            if (arg_flags.Any(lowercase_arg.StartsWith))
             {
-                Argument matching_argument = FindMatchingArgument(arg);
+                Argument matching_argument = FindMatchingArgument(lowercase_arg);
 
+                // Return if no matching command was found.
                 if (matching_argument.Names == null)
                 {
-                    error_func ??= Error.UnrecognizedArgs;
-                    error_func(arg);
-
+                    error_func(args[i]);
                     return new List<ParsedArgument>();
                 }
 
                 if (matching_argument.Is_flag)
-                    parsed_args.Add(new ParsedArgument(names: matching_argument.Names, value: "true", is_flag: matching_argument.Is_flag));
+                {
+                    parsed_args.Add(new ParsedArgument(
+                        names: matching_argument.Names,
+                        values: new string[]{"true"},
+                        is_flag: matching_argument.Is_flag,
+                        required: matching_argument.Required,
+                        min_args_length: matching_argument.Min_args_length,
+                        max_args_length: matching_argument.Max_args_length,
+                        method: matching_argument.Method
+                    ));
+                }
 
                 else
                 {
-                    string out_value;
-                    if (Array.IndexOf(args, arg) == args.Length - 1)
+                    string[] out_values;
+                    // if (Array.IndexOf(args, lowercase_arg) == args.Length - 1)
+                    if (Utils.Array.IsEmpty(args))
                     {
-                        if (matching_argument.Default_value == null)
+                        if (matching_argument.Default_values == null)
                         {
-                            new Error($"Missing value for argument: {arg}");
-                            out_value = null;
+                            Error.NoArgs(args[i]);
+                            return new List<ParsedArgument>();
                         }
 
                         else
-                            out_value = matching_argument.Default_value;
+                            out_values = matching_argument.Default_values;
                     }
 
                     else
-                        out_value = args[Array.IndexOf(args, arg) + 1];
+                        out_values = args;
 
-                    parsed_args.Add(new ParsedArgument(names: matching_argument.Names, value: out_value, is_flag: matching_argument.Is_flag));
+                    if (matching_argument.Min_args_length > 0 && Utils.Array.Reduce(out_values).Length < matching_argument.Min_args_length)
+                    {
+                        Error.TooFewArgs(Utils.Array.Reduce(out_values));
+                        return new List<ParsedArgument>();
+                    }
+
+                    else if (matching_argument.Max_args_length > 0 && Utils.Array.Reduce(out_values).Length > matching_argument.Max_args_length)
+                    {
+                        Error.TooManyArgs(Utils.Array.Reduce(out_values));
+                        return new List<ParsedArgument>();
+                    }
+
+                    parsed_args.Add(new ParsedArgument(
+                        names: matching_argument.Names,
+                        values: out_values,
+                        is_flag: matching_argument.Is_flag,
+                        required: matching_argument.Required,
+                        min_args_length: matching_argument.Min_args_length,
+                        max_args_length: matching_argument.Max_args_length,
+                        method: matching_argument.Method
+                    ));
+
                     i++;
                 }
             }
 
             else
-                parsed_args.Add(new ParsedArgument(new string[]{arg}));
+            {
+                parsed_args.Add(new ParsedArgument(
+                    names: new string[]{args[i]},
+                    values: new string[0],
+                    is_flag: true,
+                    required: false,
+                    min_args_length: 0,
+                    max_args_length: 0,
+                    method: null,
+                    known_type: "Unknown"
+                ));
+            }
+
+            List<string> missing_arg_list = arguments.Where(
+                argument => argument.Required
+                &&
+                !parsed_args.Any(arg => arg.Names.SequenceEqual(argument.Names))).Select(argument => argument.Names[0]
+            ).ToList();
+
+            if (missing_arg_list.Count > 0)
+            {
+                Error.TooFewArgs("");
+                new Error($"Missing required argument(s): {string.Join(", ", missing_arg_list)}");
+                return new List<ParsedArgument>();
+            }
         }
 
-        List<string> missing_arg_list = arguments.Where(
-            argument => argument.Required
-            &&
-            !parsed_args.Any(arg => arg.Names.SequenceEqual(argument.Names))).Select(argument => argument.Names[0]
-        ).ToList();
+        // for (int i = 0; i < args.Length; i++)
+        // {
+        //     string arg = args[i];
+        //     if (arg_flags.Any(prefix => arg.StartsWith(prefix)))
+        //     {
+        //         Argument matching_argument = FindMatchingArgument(arg);
 
-        if (missing_arg_list.Count > 0)
-        {
-            Error.TooFewArgs("");
-            new Error($"Missing required argument(s): {string.Join(", ", missing_arg_list)}");
-            return new List<ParsedArgument>();
-        }
+        //         if (matching_argument.Names == null)
+        //         {
+        //             error_func ??= Error.UnrecognizedArgs;
+        //             error_func(arg);
+
+        //             return new List<ParsedArgument>();
+        //         }
+
+        //         if (matching_argument.Is_flag)
+        //             parsed_args.Add(new ParsedArgument(names: matching_argument.Names, value: "true", is_flag: matching_argument.Is_flag));
+
+        //         else
+        //         {
+        //             string out_value;
+        //             if (Array.IndexOf(args, arg) == args.Length - 1)
+        //             {
+        //                 if (matching_argument.Default_value == null)
+        //                 {
+        //                     new Error($"Missing value for argument: {arg}");
+        //                     out_value = null;
+        //                 }
+
+        //                 else
+        //                     out_value = matching_argument.Default_value;
+        //             }
+
+        //             else
+        //                 out_value = args[Array.IndexOf(args, arg) + 1];
+
+        //             parsed_args.Add(new ParsedArgument(names: matching_argument.Names, value: out_value, is_flag: matching_argument.Is_flag));
+        //             i++;
+        //         }
+        //     }
+
+        //     else
+        //         parsed_args.Add(new ParsedArgument(new string[]{arg}));
+        // }
+
+        // List<string> missing_arg_list = arguments.Where(
+        //     argument => argument.Required
+        //     &&
+        //     !parsed_args.Any(arg => arg.Names.SequenceEqual(argument.Names))).Select(argument => argument.Names[0]
+        // ).ToList();
+
+        // if (missing_arg_list.Count > 0)
+        // {
+        //     Error.TooFewArgs("");
+        //     new Error($"Missing required argument(s): {string.Join(", ", missing_arg_list)}");
+        //     return new List<ParsedArgument>();
+        // }
 
         return parsed_args;
     }
--- src/utils/Argparse/Structs/ParsedArgument.cs
+++ src/utils/Argparse/Structs/ParsedArgument.cs
@@ -1,16 +1,26 @@
 partial class Argparse
 {
-   public struct ParsedArgument
+    public struct ParsedArgument
     {
         public string[] Names { get; set; }
-        public string Value { get; set; }
+        public string[] Values { get; set; }
         public bool Is_flag { get; set; }
+        public bool Required { get; set; }
+        public int Max_args_length { get; set; }
+        public int Min_args_length { get; set; }
+        public Delegate Method { get; set; }
+        public string KnownType { get; set; } = "Known";
 
-        public ParsedArgument(string[] names=null, string value=null, bool is_flag=false)
+        public ParsedArgument(string[] names, string[] values, bool is_flag, bool required, int min_args_length, int max_args_length, Delegate method, string known_type="Known")
         {
             this.Names = names ?? new string[0];
-            this.Value = value;
+            this.Values = values;
             this.Is_flag = is_flag;
+            this.Required = required;
+            this.Max_args_length = max_args_length;
+            this.Min_args_length = min_args_length;
+            this.Method = method;
+            this.KnownType = known_type;
         }
     }
 }
