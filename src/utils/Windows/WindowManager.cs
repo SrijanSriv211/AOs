227795f3d0e415895bc2c190a66a3394a8860061
Light-Lens <QCoreNest@gmail.com>
2023-10-01 00:23:29 +0530

    Add WindowManager

---
+++ src/utils/Windows/WindowManager.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices;
+
+class WindowManager
+{
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+}
