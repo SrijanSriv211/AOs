5a2076b0847b01af68d82d08121efccdb294a702
Light-Lens <QCoreNest@gmail.com>
2023-10-02 18:46:24 +0530

    change colors in UPR

--- src/AOs/UPR.py
+++ src/AOs/UPR.py
@@ -9,18 +9,20 @@ response = requests.get("https://api.github.com/repos/Light-Lens/AOs/releases/la
 # Get all the required json data
 tag_name = response.json()["tag_name"]
 assets = response.json()["assets"]
+download_link = assets[1]["browser_download_url"]
 current_version = sys.argv[1]
 
 # assign the first asset's name that is a digit to the latest_version variable. If no such asset is found, latest_version will be set to None.
 latest_version = next((asset["name"] for asset in assets if asset["name"].isdigit()), None)
 
-if current_version >= latest_version:
+if int(current_version) >= int(latest_version):
     print("You're up to date.")
 
 else:
     print("Updates are available.")
 
-    print(f"Your version: {Fore.BLUE}{Style.BRIGHT}{current_version}")
-    print(f"Latest version: {Fore.BLUE}{Style.BRIGHT}{latest_version}")
+    print(f"Your version: {Fore.WHITE}{Style.BRIGHT}{current_version}")
+    print(f"Latest version: {Fore.WHITE}{Style.BRIGHT}{latest_version}")
 
-    print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}https://github.com/Light-Lens/AOs/releases/download/{tag_name}/AOs.zip")
+    # print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}https://github.com/Light-Lens/AOs/releases/download/{tag_name}/AOs.zip")
+    print(f"Get the Latest version here: {Fore.CYAN}{Style.BRIGHT}{download_link}")
