cacdf1fc9ca6a0b16b926ca45144333da78702c5
Light-Lens <QCoreNest@gmail.com>
2023-10-13 18:44:28 +0530

    Add test print statements

--- src/AOs/Features/DeveloperFeatures/Features/DF_CreateNewProject.cs
+++ src/AOs/Features/DeveloperFeatures/Features/DF_CreateNewProject.cs
@@ -3,5 +3,7 @@ partial class DeveloperFeatures
     private void New(string[] args)
     {
         Console.WriteLine("Create new proj");
+        Console.WriteLine(string.Join(", ", args));
+        Console.WriteLine();
     }
 }
--- src/AOs/Features/DeveloperFeatures/Features/DF_RunExternalApps.cs
+++ src/AOs/Features/DeveloperFeatures/Features/DF_RunExternalApps.cs
@@ -3,10 +3,14 @@ partial class DeveloperFeatures
     private void RunCLOC(string[] args)
     {
         Console.WriteLine("Run CLOC");
+        Console.WriteLine(string.Join(", ", args));
+        Console.WriteLine();
     }
 
     private void RunGit(string[] args)
     {
         Console.WriteLine("Run GIT");
+        Console.WriteLine(string.Join(", ", args));
+        Console.WriteLine();
     }
 }
