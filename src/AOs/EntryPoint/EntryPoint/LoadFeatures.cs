4100a8fe1e4f9f8419d358627b48d6e50674e6bb
Light-Lens <QCoreNest@gmail.com>
2023-10-26 19:44:53 +0530

    Remove developer features

--- src/AOs/EntryPoint/EntryPoint/LoadFeatures.cs
+++ src/AOs/EntryPoint/EntryPoint/LoadFeatures.cs
@@ -138,20 +138,21 @@ partial class EntryPoint
             default_values: new string[0]{}, max_args_length: 2, method: this.features.ControlVolume
         );
 
+        //* NOTE Developer commands are now a part of AOs 2.6 development plan.
         // Developer commands
-        this.parser.Add(
-            new string[]{ "dev", "developer" }, "Developer tools",
-            supported_args: new Dictionary<string[], string>
-            {
-                {new string[]{"-h", "--help"}, "Show information about a developer command"},
-                {new string[]{"new"}, "Create a new project"},
-                {new string[]{"git", "github"}, "Use git to maintain version control"},
-                {new string[]{"cloc", "countlinesofcode"}, "Count the lines of code in a project directory"},
-                {new string[]{"server"}, "Start a local web-server"},
-                {new string[]{"clean"}, "Delete temp/unnecessary files created by the programming language in the project"},
-                {new string[]{"ver", "version"}, "Show the current build number of the project"},
-            },
-            is_flag: false, method: this.features.DevCMD
-        );
+        // this.parser.Add(
+        //     new string[]{ "dev", "developer" }, "Developer tools",
+        //     supported_args: new Dictionary<string[], string>
+        //     {
+        //         {new string[]{"-h", "--help"}, "Show information about a developer command"},
+        //         {new string[]{"new"}, "Create a new project"},
+        //         {new string[]{"git", "github"}, "Use git to maintain version control"},
+        //         {new string[]{"cloc", "countlinesofcode"}, "Count the lines of code in a project directory"},
+        //         {new string[]{"server"}, "Start a local web-server"},
+        //         {new string[]{"clean"}, "Delete temp/unnecessary files created by the programming language in the project"},
+        //         {new string[]{"ver", "version"}, "Show the current build number of the project"},
+        //     },
+        //     is_flag: false, method: this.features.DevCMD
+        // );
     }
 }
