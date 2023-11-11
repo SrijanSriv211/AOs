bde5b2e6ea4b061c82628ea4907f415ee0ebbb1a
Light-Lens <QCoreNest@gmail.com>
2023-11-11 14:26:59 +0530

    Create DF_JsonObj.cs

---
+++ src/AOs/Features/DeveloperFeatures/DF_JsonObj.cs
@@ -0,0 +1,26 @@
+partial class DeveloperFeatures
+{
+    public class ProjectTemplate
+    {
+        public string project_name { get; set; }
+        public string project_path { get; set; }
+        public string github_URL { get; set; }
+        public string github_branch { get; set; }
+        public int build { get; set; }
+        public List<string> clean_project_waste { get; set; }
+    }
+
+    public class TasksDetails
+    {
+        public string description { get; set; }
+        public string args { get; set; }
+        public bool is_flag { get; set; }
+        public bool update_build_number { get; set; }
+        public List<string> call_other_tasks { get; set; }
+    }
+
+    public class TasksConfig
+    {
+        public Dictionary<string, TasksDetails> tasks { get; set; }
+    }
+}
