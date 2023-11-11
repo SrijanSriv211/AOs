4a648003e2b5e607bd5791a81843c68a15e88a68
Light-Lens <QCoreNest@gmail.com>
2023-11-11 14:26:50 +0530

    Print an error occured when system crashes.

--- src/AOs/EntryPoint/Logging/Logging.cs
+++ src/AOs/EntryPoint/Logging/Logging.cs
@@ -3,9 +3,10 @@ partial class EntryPoint
     public static void CrashreportLog(string content)
     {
         string current_time = $"[{DateTime.Now:h:mm:ss tt} {DateTime.Now:dddd, dd MMMM yyyy}]";
-        string boot_log_path = Path.Combine(Obsidian.root_dir, "Files.x72\\root\\log\\Crashreport.log");
+        string crashreport_path = Path.Combine(Obsidian.root_dir, "Files.x72\\root\\log\\Crashreport.log");
 
-        FileIO.FileSystem.Write(boot_log_path, $"{current_time}\t{content} \n");
+        TerminalColor.Print($"An error occured, for more information see the crash report at '{crashreport_path}'", ConsoleColor.Red);
+        FileIO.FileSystem.Write(crashreport_path, $"{current_time}\t{content} \n");
     }
 
     private static void StartupLog(string content)
