3344ff817cb464ca4d24891d8a3b575d2879dfbb
Light-Lens <QCoreNest@gmail.com>
2024-03-31 12:39:02 +0530

    Check if AOs is running in Windows Terminal or not using WT_SESSION env var

--- src/AOs/Obsidian/ClearConsole.cs
+++ src/AOs/Obsidian/ClearConsole.cs
@@ -5,5 +5,13 @@ partial class Obsidian
         sys_utils.CommandPrompt("cls");
         TerminalColor.Print(version, ConsoleColor.Yellow, false);
         TerminalColor.Print($"  ({Environment.GetEnvironmentVariable("username")})", ConsoleColor.White);
+
+        // https://stackoverflow.com/a/72575526/18121288
+        // Check if AOs is running in Windows Terminal or using using the environment variable %WT_SESSION%
+        // If AOs is running in Windows Terminal in it will return a terminal session id,
+        // if not then it will return an empty string pointing that AOs is running in Windows Console Host or some other terminal,
+        // and if that's the case then suggest the user to use AOs in Windows Terminal for better experience.
+        if (Utils.String.IsEmpty(Environment.GetEnvironmentVariable("WT_SESSION")))
+            TerminalColor.Print("For better experience please run AOs in Windows Terminal.", ConsoleColor.DarkRed);
     }
 }
