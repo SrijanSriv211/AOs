f6fd389cafc7da9cf8649f3f5985dc2c96b5de3b
Light-Lens <QCoreNest@gmail.com>
2024-04-03 19:10:15 +0530

    Last token was not being appended to the List. Fixed it.

--- src/AOs/Lexer/Tokenizer/Tokenizer.cs
+++ src/AOs/Lexer/Tokenizer/Tokenizer.cs
@@ -15,50 +15,16 @@ namespace Lexer
             // Loop through all chars in the line.
             for (i = 0; i < line.Length; i++)
             {
-                // '#' means that the following text is a comment.
-                if (tok == "#")
+                int tok_state = CheckForToken(tok);
+                if (tok_state == -1)
                     break;
 
-                else if (Utils.String.IsWhiteSpace(tok))
-                    ClearToken(); // Remove whitespaces from the line.
-
-                // ';' will be used to separate two different commands.
-                // Useful for multiple commands in single line.
-                else if (tok == ";")
-                    AppendToken(TokenType.EOL);
-
-                else if (">@!".Contains(tok.FirstOrDefault()))
-                    AppendToken(TokenType.SYMBOL);
-
-                else if (IsIdentifier(tok.FirstOrDefault()) && !IsExpr(tok.FirstOrDefault()) && tok.Length == 1)
-                {
-                    AdvanceChar(IsIdentifier);
-                    AppendToken(EntryPoint.AllCommands.Contains(tok.ToLower()) ? TokenType.KEYWORD : TokenType.IDENTIFIER);
-                }
-
-                else if (IsExpr(tok.FirstOrDefault()) && tok.Length == 1)
-                {
-                    AdvanceChar(IsExprWithLetter);
-                    // If the token contains any letter then it's an identifier.
-                    // Remove whitespace from token only when it's an expr.
-                    tok = tok.Any(char.IsLetter) ? tok : tok.Replace(" ", "");
-                    AppendToken(tok.Any(char.IsLetter) ? TokenType.IDENTIFIER : TokenType.EXPR);
-                }
-
-                else if (tok == "\"" || tok == "'")
-                {
-                    if (!MakeString(tok.FirstOrDefault()))
-                    {
-                        tokens = [];
-                        break;
-                    }
-
-                    AppendToken(TokenType.STRING);
-                }
-
-                else
+                else if (tok_state == 0)
                     tok += line[i];
             }
+
+            if (!Utils.String.IsEmpty(tok))
+                CheckForToken(tok);
 
             tok = "";
             AppendToken(TokenType.EOL);
@@ -89,6 +55,64 @@ namespace Lexer
         {
             AdvanceChar(func);
             AppendToken(type);
+        }
+
+        private int CheckForToken(string tok)
+        {
+            // '#' means that the following text is a comment.
+            if (tok == "#")
+                return -1;
+
+            else if (Utils.String.IsWhiteSpace(tok))
+            {
+                ClearToken(); // Remove whitespaces from the line.
+                return 1;
+            }
+
+            // ';' will be used to separate two different commands.
+            // Useful for multiple commands in single line.
+            else if (tok == ";")
+            {
+                AppendToken(TokenType.EOL);
+                return 1;
+            }
+
+            else if (">@!".Contains(tok.FirstOrDefault()))
+            {
+                AppendToken(TokenType.SYMBOL);
+                return 1;
+            }
+
+            else if (IsIdentifier(tok.FirstOrDefault()) && !IsExpr(tok.FirstOrDefault()) && tok.Length == 1)
+            {
+                AdvanceChar(IsIdentifier);
+                AppendToken(TokenType.IDENTIFIER);
+                return 1;
+            }
+
+            else if (IsExpr(tok.FirstOrDefault()) && tok.Length == 1)
+            {
+                AdvanceChar(IsExprWithLetter);
+                // If the token contains any letter then it's an identifier.
+                // Remove whitespace from token only when it's an expr.
+                tok = tok.Any(char.IsLetter) ? tok : tok.Replace(" ", "");
+                AppendToken(tok.Any(char.IsLetter) ? TokenType.IDENTIFIER : TokenType.EXPR);
+                return 1;
+            }
+
+            else if (tok == "\"" || tok == "'")
+            {
+                if (!MakeString(tok.FirstOrDefault()))
+                {
+                    tokens = [];
+                    return -1;
+                }
+
+                AppendToken(TokenType.STRING);
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
