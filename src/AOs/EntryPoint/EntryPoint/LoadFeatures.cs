cedb4be035e1de1bab9d5490ebfc19e2bbc893a3
Light-Lens <QCoreNest@gmail.com>
2024-04-04 17:18:14 +0530

    Remove experimental commands

--- src/AOs/EntryPoint/EntryPoint/LoadFeatures.cs
+++ src/AOs/EntryPoint/EntryPoint/LoadFeatures.cs
@@ -1,64 +1,9 @@
 partial class EntryPoint
 {
-    private void LoadFeatures()
+    // A dictionary containng all the the method names from settings.json alongwith their respective functions.
+    private Dictionary<string, Delegate> GetInternalMethod()
     {
-        /*
-        * The following features which are not available in settings.json are immutable, meaning they can't be changed in anyway unless the code is modified.
-          These features are immutable because they are in some sense the USP of AOs meaning these features are what makes AOs different from other Terminals.
-          Some features are fundamental for AOs though such as the quit, version, about or credits.
-          The rest of the features will be mutable and can be changed at any time.
-        * NOTE: After modifying settings.json AOs needs to be reloaded to apply those changes.
-        */
-
-        // Experimental commands
-        parser.Add(
-            ["@"], "Experimental/Overload commands",
-            supported_args: new Dictionary<string[], string>
-            {
-                {["-h", "--help"], "Show information about a developer command"},
-                {["itsmagic"], "It's magic, it's magic."},
-                {["switch"], "Switch between applications using the App IDs"},
-                {["studybyte"], "Starts Studybyte"},
-                {["cpix"], "Starts Cpix"}
-            },
-            default_values: ["help"], is_flag: false, method: features.ExperimentCMD
-        );
-
-        // Default-else shell command
-        parser.Add(
-            ["!"], "Switch the default-else shell",
-            supported_args: new Dictionary<string[], string>
-            {
-                {["cmd"], "Switch the default-else shell to cmd.exe"},
-                {["ps", "powershell"], "Switch the default-else shell to powershell.exe"}
-            },
-            default_values: [""], max_args_length: 1, method: features.SwitchElseShell
-        );
-
-        // Developer commands
-        parser.Add(
-            ["dev", "developer"], "Developer tools",
-            supported_args: new Dictionary<string[], string>
-            {
-                {["-h", "--help"], "Show information about a developer command"},
-                {["new"], "Create a new project"},
-                {["-t", "tasks"], "List or run custom task in the developer environment"},
-                {["clean"], "Delete temporary/unnecessary files created in the project"},
-                {["-v", "ver", "version"], "Show the current build number of the project"},
-            },
-            default_values: ["help"], is_flag: false, method: features.DevCMD
-        );
-
-        // Flagged commands
-        parser.Add(["settings"], "Displays the AOs settings", method: features.PrintAOsSettings);
-        parser.Add(["version", "ver", "-v"], "Displays the AOs version", method: features.PrintVersion);
-        parser.Add(["about", "info"], "About AOs", method: features.About);
-        parser.Add(["reload", "refresh"], "Restart AOs", method: features.Refresh);
-        parser.Add(["credits"], "Credit for AOs", method: Obsidian.Credits);
-        parser.Add(["quit", "exit"], "Exit AOs", method: features.Exit);
-
-        // A dictionary containng all the the method names from settings.json alongwith their respective functions.
-        Dictionary<string, Delegate> internal_methods = new() {
+        return new Dictionary<string, Delegate>() {
             { "RunAOsAsAdmin", features.Admin },
             { "ClearConsole", AOs.ClearConsole },
             { "ShutdownHost", features.Shutdown },
@@ -93,7 +38,67 @@ partial class EntryPoint
             { "TerminateRunningProcess", features.Terminate },
             { "Filer", features.Filer },
             { "ControlVolume", features.ControlVolume },
+            { "ItsMagic", features.ItsMagic },
+            { "SwitchApp", features.SwitchApp },
         };
+    }
+
+    private void LoadFeatures()
+    {
+        /*
+        * The following features which are not available in settings.json are immutable, meaning they can't be changed in anyway unless the code is modified.
+          These features are immutable because they are in some sense the USP of AOs meaning these features are what makes AOs different from other Terminals.
+          Some features are fundamental for AOs though such as the quit, version, about or credits.
+          The rest of the features will be mutable and can be changed at any time.
+        * NOTE: After modifying settings.json AOs needs to be reloaded to apply those changes.
+        */
+
+        // Experimental commands
+        // parser.Add(
+        //     ["@"], "Experimental/Overload commands",
+        //     supported_args: new Dictionary<string[], string>
+        //     {
+        //         {["-h", "--help"], "Show information about a developer command"},
+        //         {["itsmagic"], "It's magic, it's magic."},
+        //         {["switch"], "Switch between applications using the App IDs"},
+        //         {["studybyte"], "Starts Studybyte"},
+        //         {["cpix"], "Starts Cpix"}
+        //     },
+        //     default_values: ["help"], is_flag: false, method: features.ExperimentCMD
+        // );
+
+        // Default-else shell command
+        parser.Add(
+            ["!"], "Switch the default-else shell",
+            supported_args: new Dictionary<string[], string>
+            {
+                {["cmd"], "Switch the default-else shell to cmd.exe"},
+                {["ps", "powershell"], "Switch the default-else shell to powershell.exe"}
+            },
+            default_values: [""], max_args_length: 1, method: features.SwitchElseShell
+        );
+
+        // Developer commands
+        parser.Add(
+            ["dev", "developer"], "Developer tools",
+            supported_args: new Dictionary<string[], string>
+            {
+                {["-h", "--help"], "Show information about a developer command"},
+                {["new"], "Create a new project"},
+                {["-t", "tasks"], "List or run custom task in the developer environment"},
+                {["clean"], "Delete temporary/unnecessary files created in the project"},
+                {["-v", "ver", "version"], "Show the current build number of the project"},
+            },
+            default_values: ["help"], is_flag: false, method: features.DevCMD
+        );
+
+        // Flagged commands
+        parser.Add(["settings"], "Displays the AOs settings", method: features.PrintAOsSettings);
+        parser.Add(["version", "ver", "-v"], "Displays the AOs version", method: features.PrintVersion);
+        parser.Add(["about", "info"], "About AOs", method: features.About);
+        parser.Add(["reload", "refresh"], "Restart AOs", method: features.Refresh);
+        parser.Add(["credits"], "Credit for AOs", method: Obsidian.Credits);
+        parser.Add(["quit", "exit"], "Exit AOs", method: features.Exit);
 
         // Load all the features from settings.json
         foreach (var cmd in Settings.cmds)
@@ -109,7 +114,7 @@ partial class EntryPoint
                     is_flag: cmd.is_flag,
                     min_args_length: cmd.min_arg_len,
                     max_args_length: cmd.max_arg_len,
-                    method: internal_methods[cmd.method]
+                    method: GetInternalMethod()[cmd.method]
                     // index_cmd: cmd.index_cmd
                 );
             }
