27cd592734dd5cdf293a62c4678871f483996850
Light-Lens <QCoreNest@gmail.com>
2024-04-06 21:43:38 +0530

    Rework build.py

--- scripts/build.py
+++ scripts/build.py
@@ -1,101 +1,53 @@
-import PyInstaller.__main__, shutil, sys, os
+import shutil, sys, os
 
-def rmdirs(*folders):
-    for i in folders:
-        if os.path.exists(i):
-            shutil.rmtree(i)
+# remove folders from the root dir
+def rmdirs(folders):
+    [shutil.rmtree(i) for i in folders if os.path.exists(i)]
 
+# get the current build number of AOs
 def get_build_no():
-    if os.path.isfile("scripts\\build.txt"):
-        return str(int(open("scripts\\build.txt", "r").read()) + 1)
+    return open("scripts\\build.txt", "r").read() if os.path.isfile("scripts\\build.txt") else "0"
 
-    return "0"
-
+# Update the build number of AOs
 def update_build_no():
-    build_no = get_build_no()
+    build_no = str(int(get_build_no()) + 1) # increment +1 to the current build no of AOs
+
     with open("scripts\\build.txt", "w") as f:
         f.write(build_no)
 
     return build_no
 
-def build_UPR(output_dir):
-    options = [
-        "src\\AOs\\UPR.py",
-        "--distpath", output_dir,
-        "--icon", "img\\UPR.ico",
-        "--clean",
-        "--onefile"
-    ]
+# run AOs
+def run_AOs(build_no):
+    os.system(f"dotnet run -p:FileVersion=2.6.{build_no} -- {' '.join(sys.argv[2:])}")
 
-    PyInstaller.__main__.run(options)
-
-    rmdirs("build")
-    os.remove("UPR.spec")
-
-def build_Filer(output_dir):
-    options = [
-        "src\\vendor\\Filer\\filer_cli.py",
-        "--distpath", output_dir,
-        "--icon", "src\\vendor\\Filer\\img\\logo.ico",
-        "--clean",
-        "--onefile"
-    ]
-
-    PyInstaller.__main__.run(options)
-
-    rmdirs("build")
-    os.remove("filer_cli.spec")
-
-def run_AOs(argv, build_no):
-    if os.path.isfile("bin\\Debug\\net8.0\\UPR.exe") == False:
-        build_UPR("bin\\Debug\\net8.0")
-
-    if os.path.isfile("bin\\Debug\\net8.0\\filer_cli.exe") == False:
-        build_Filer("bin\\Debug\\net8.0")
-
-    if argv[2:]:
-        os.system(f"dotnet run -p:FileVersion=2.6.{build_no} -- {' '.join(argv[2:])}")
-
-    else:
-        os.system(f"dotnet run -p:FileVersion=2.6.{build_no}")
-
+# Build AOs in publish mode.
 def build_AOs():
-    rmdirs("AOs")
     if os.path.exists("AOs") == False:
         os.mkdir("AOs")
 
     os.system(f"dotnet publish --self-contained -p:FileVersion=2.6.{update_build_no()} -c Release -o ./AOs")
-    build_UPR("AOs")
-    build_Filer("AOs")
+    rmdirs(["bin", "obj"])
 
 if len(sys.argv) > 1:
     # print help message
     if sys.argv[1] == "help" or sys.argv[1] == "-h" or sys.argv[1] == "--help" or sys.argv[1] == "-?" or sys.argv[1] == "??":
         print("If no argument is passed     -> Build AOs from source")
-        print("execute                      -> Run AOs release executable")
         print("clean                        -> Remove 'bin', 'obj' folders from the root directory.")
         print("srun                         -> Run AOs but don't update the build no.")
         print("run                          -> Run AOs and update the build no.")
 
     # delete the following folder
     elif sys.argv[1] == "clean":
-        rmdirs("bin", "obj", "build")
+        rmdirs(["bin", "obj"])
 
     # run and update the build no.
     elif sys.argv[1] == "run":
-        run_AOs(sys.argv, update_build_no())
+        run_AOs(update_build_no())
 
     # 'srun' -> silent run, run but don't update the build no.
     elif sys.argv[1] == "srun":
-        run_AOs(sys.argv, get_build_no())
-
-    elif sys.argv[1] == "execute":
-        if os.path.isfile("./AOs/AOs.exe") == False:
-            print("Building AOs")
-            build_AOs()
-
-        print("Executing AOs")
-        os.system(f".\\AOs\\AOs.exe {' '.join(sys.argv[2:])}")
+        run_AOs(get_build_no())
 
 else:
     build_AOs()
