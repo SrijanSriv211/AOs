0d35146f7aa7e80e299d5ad89cb16f5ae03531c3
Light-Lens <QCoreNest@gmail.com>
2024-05-17 17:20:50 +0530

    Add a math method to split a number by decimal point

--- src/shared/Utils/Maths.cs
+++ src/shared/Utils/Maths.cs
@@ -6,5 +6,14 @@ namespace Utils
         {
             return Math.Max(max_padding_len - (int)Math.Log10(count), 0);
         }
+
+        // Split the number into 2 parts (before and after the decimal point)
+        public static (int, double) SplitNumber(double Number)
+        {
+            int BeforeDecimalPart = (int)Number;
+            double FractionalPart = Number - BeforeDecimalPart;
+
+            return (BeforeDecimalPart, FractionalPart);
+        }
     }
 }
