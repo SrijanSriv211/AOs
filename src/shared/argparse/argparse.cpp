aa78bfae07c25495fe65105a85897fceb8cdbfb8
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-11 22:07:21 +0530

    Add help methods in argparse to show details of arguments

--- src/shared/argparse/argparse.cpp
+++ src/shared/argparse/argparse.cpp
@@ -1,7 +1,10 @@
 #include "aospch.h"
 #include "argparse.h"
+
 #include "strings/strings.h"
 #include "console/console.h"
+#include "array/array.h"
+#include "math/math.h"
 
 argparse::argparse(const std::string& name, const std::string& description, const std::function<void(std::string)>& error_func)
 {
@@ -97,4 +100,88 @@ std::vector<argparse::parsed_argument> argparse::parse(const std::vector<std::st
     }
 
     return parsed_args;
+}
+
+void argparse::print_help()
+{
+    console::print("Name:", console::color::LIGHT_YELLOW);
+    std::cout << this->name << "\n\n";
+
+    console::print("Description:", console::color::LIGHT_CYAN);
+    std::cout << this->description << "\n\n";
+
+    console::print("Usage:", console::color::WHITE);
+    std::cout << this->name << " [OPTIONS]" << "\n\n";
+
+    console::print("Options:", console::color::MAGENTA);
+    for (int i = 0; i < arguments.size(); i++)
+    {
+        argparse::argument arg = arguments[i];
+
+        std::string arg_name = strings::join(", ", array::reduce(arg.names));
+        std::string default_value = (!arg.default_value.empty()) ? " (default: " + arg.default_value + ")" : "";
+        std::string is_required = (arg.required) ? " (required: true)" : " (required: false)";
+        std::string is_flag = (arg.is_flag) ? " (is flag: true)" : " (is flag: false)";
+
+        int padding = math::calc_padding(1);
+        std::cout << std::setw(padding) << std::left << arg_name << "  ";
+        console::print(arg.help + default_value + is_required + is_flag, console::color::GRAY);
+    }
+
+    std::cout << std::endl;
+}
+
+void argparse::print_help(const argument& details)
+{
+    std::string names = strings::join(", ", details.names);
+    std::string description = details.help;
+    std::string default_value = (!details.default_value.empty() && !strings::is_empty(details.default_value)) ? " (default: " + details.default_value + ")" : "";
+    std::string is_flag = details.is_flag ? " (is flag: true)" : " (is flag: false)";
+    std::string is_required = details.required ? " (required: true)" : " (required: false)";
+
+    console::print("Name:", console::color::LIGHT_CYAN);
+    int padding = math::calc_padding(1);
+    std::cout << std::setw(padding) << std::left << names << "  ";
+    console::print(description + "\n", console::color::GRAY);
+
+    console::print("Details:", console::color::LIGHT_BLUE);
+    std::cout << names << " [OPTIONS] " << default_value << is_flag << is_required << "\n\n";
+}
+
+void argparse::get_help(const std::vector<std::string>& cmd_names)
+{
+    const std::vector<std::string> names = array::reduce(cmd_names);
+
+    if (array::is_empty(names))
+    {
+        std::cout << "Type `help <command-name>` for more information on a specific command" << "\n\n";
+
+        for (int i = 0; i < arguments.size(); i++)
+        {
+            argparse::argument detail = arguments[i];
+
+            std::vector<std::string> command_names = detail.names;
+            std::string description = detail.help;
+            int padding = math::calc_padding(i+1);
+
+            console::print(i+1 + ". ", console::color::GRAY, false);
+            std::cout << std::setw(padding) << std::left << strings::join(", ", command_names) << "  ";
+            console::print(description, console::color::GRAY);
+        }
+    }
+
+    else
+    {
+        for (const std::string name : names)
+        {
+            argument matching_cmd = find_matching_argument(name);
+            if (matching_cmd.names.empty())
+            {
+                console::print("No information for command '" + name + "'", console::color::LIGHT_RED);
+                continue;
+            }
+
+            print_help(matching_cmd);
+        }
+    }
 }
