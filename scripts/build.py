b2538a60a073baafe10a5315afdc63eb9793ce15
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-13 18:59:59 +0530

    Clean up build.py

--- scripts/build.py
+++ scripts/build.py
@@ -1,53 +1,33 @@
 import shutil, sys, os
 
+def get_build_no():
+    return int(open("scripts\\build.txt", "r").read()) if os.path.isfile("scripts\\build.txt") else 0
+
+def update_build_no():
+    build_no = str(get_build_no() + 1) # increment +1 to the current build no of AOs
+    open("scripts\\build.txt", "w").write(build_no)
+
 def precompile_files():
     filedata = [
         ("src/aospch.h.gch", "g++ src/aospch.h"), # compile AOs precompiled headers
         ("src/ico.o", "windres src/ico.rc -O coff -o src/ico.o") # create ico.o containing the data for AOs icon
     ]
 
-    for path, cmd in filedata:
-        if os.path.isfile(path) == False:
-            os.system(cmd)
-
-def get_build_no():
-    return int(open("scripts\\build.txt", "r").read()) if os.path.isfile("scripts\\build.txt") else 0
-
-# increment +1 to the current build no of AOs
-def update_build_no():
-    build_no = str(get_build_no() + 1)
-    open("scripts\\build.txt", "w").write(build_no)
+    [os.system(cmd) for path, cmd in filedata if os.path.isfile(path) == False]
 
 # https://stackoverflow.com/a/2909998/18121288
-def compile_AOs():
-    cpp_dirs = []
-    for path, subdirs, files in os.walk("."):
-        for name in files:
-            if (name.endswith(".cpp")):
-                cpp_dirs.append(path + "\\*.cpp")
-                break
-
-    # compile AOs
-    all_src_files = " ".join(cpp_dirs)
-    script = f"g++ src/ico.o {all_src_files} -DVERSION=2.8 -DBUILD_NUMBER={get_build_no()} -Isrc/ -Isrc/shared/ -std=c++20 -o bin/AOs.exe"
-    os.system(script)
-
-def run_AOs(*arguments):
-    os.system(f"bin\\AOs.exe {" ".join(arguments[0])}")
-
-# remove folders from the root dir
-def rm(folders, files):
-    [shutil.rmtree(i) for i in folders if os.path.exists(i)]
-    [os.remove(i) for i in files if os.path.isfile(i)]
+SRC_FILES = " ".join([os.path.join(path, "*.cpp") for path, _, files in os.walk("src") if any(name.endswith(".cpp") for name in files)])
+INCLUDE_DIRS = "-Isrc/ -Isrc/shared/"
+SCRIPT = f"g++ src/ico.o {SRC_FILES} {INCLUDE_DIRS} -DVERSION=2.8 -DBUILD_NUMBER={get_build_no()} -std=c++20 -o bin/AOs.exe"
 
 # create the bin folder
 if os.path.isdir("bin") == False:
     os.mkdir("bin")
 
 if not sys.argv[1:]:
-    precompile_files()
-    compile_AOs()
     update_build_no()
+    precompile_files()
+    os.system(SCRIPT)
 
 for i, x in enumerate(sys.argv[1:]):
     if x == "help":
@@ -58,14 +38,15 @@ for i, x in enumerate(sys.argv[1:]):
         print("exec                         -> Execute AOs without compiling")
 
     elif x == "clean":
-        rm(folders=["bin"], files=["src/aospch.h.gch", "src/ico.o"])
+        [shutil.rmtree(i) for i in ["bin"] if os.path.exists(i)]
+        [os.remove(i) for i in ["src/aospch.h.gch", "src/ico.o"] if os.path.isfile(i)]
 
     elif x == "run":
-        compile_AOs()
-        run_AOs(sys.argv[i+2:])
+        os.system(SCRIPT)
+        os.system(f"bin\\AOs.exe {" ".join(sys.argv[i+2:])}")
 
     elif x == "exec":
-        run_AOs(sys.argv[i+2:])
+        os.system(f"bin\\AOs.exe {" ".join(sys.argv[i+2:])}")
 
     elif x == "pch":
         precompile_files()
