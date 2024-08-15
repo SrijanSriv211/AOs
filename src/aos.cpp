0f14cc4270da5893f7910b05558365f696d8239c
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-15 22:57:49 +0530

    Create a clear console function in AOs namespace

--- src/aos.cpp
+++ src/aos.cpp
@@ -1,8 +1,19 @@
 #include "aospch.h"
 #include "aos.h"
 
+#include "console/console.h"
+
+// https://stackoverflow.com/a/5459929/18121288
+// only the first 2 macros for useful for me
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
 namespace AOs
 {
+    const std::string about_AOs = "A command-line tool built to control your OS directly through the command-line";
+    const std::string AOs_repo_link = "https://github.com/SrijanSriv211/AOs";
+    const std::filesystem::path aos_env_path = std::filesystem::current_path();
+
     // https://stackoverflow.com/q/50889647/18121288
     std::string get_root_path()
     {
@@ -16,6 +27,22 @@ namespace AOs
         return (pos == std::string::npos) ? "" : std::string(buffer).substr(0, pos);
     }
 
-    std::string about_AOs = "A command-line tool built to control your OS directly through the command-line";
-    std::string AOs_repo_link = "https://github.com/SrijanSriv211/AOs";
+    void clear_console()
+    {
+        system("cls");
+        std::string AOs_version = STR(VERSION);
+        console::print("AOs 2024 [Version " + AOs_version + "]  ", console::color::LIGHT_YELLOW, false);
+        console::print("(" + std::string(std::getenv("username")) + ")", console::color::LIGHT_WHITE);
+
+        // ask user to use AOs in Windows Terminal if they are not already
+        const char* env_var_val = std::getenv("WT_SESSION");
+        if (env_var_val == nullptr || env_var_val[0] == '\0')
+        {
+            console::print("> ", console::color::GRAY, false);
+            console::print("Please use AOs in Windows Terminal for better experience.", console::color::RED);
+        }
+
+        console::print(std::filesystem::current_path().string(), console::color::LIGHT_WHITE);
+        console::print("$ ", console::color::LIGHT_WHITE, false);
+    }
 }
--- src/aos.h
+++ src/aos.h
@@ -2,7 +2,9 @@
 
 namespace AOs
 {
+    extern const std::string about_AOs;
+    extern const std::string AOs_repo_link;
+    extern const std::filesystem::path aos_env_path;
     std::string get_root_path();
-    extern std::string about_AOs;
-    extern std::string AOs_repo_link;
+    void clear_console();
 }
