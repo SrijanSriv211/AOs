280d67b6780068b4ea25b1c958f0b8d108f10666
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-16 02:51:22 +0530

    A lot more helper funcs for renderer

--- src/core/readf/helper.cpp
+++ src/core/readf/helper.cpp
@@ -20,6 +20,29 @@ namespace console
         return {0, 0}; // else return this
     }
 
+    COORD readf::calc_xy_coord(const int& total_dist)
+    {
+        // calculate the exact x and y positions to put the cursor at.
+        short y = total_dist / this->console_window_width();
+        short x = total_dist - (y * this->console_window_width());
+
+        return {x, y};
+    }
+
+    int readf::console_window_width()
+    {
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+        return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    }
+
+    int readf::console_window_height()
+    {
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+        return csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    }
+
     // https://stackoverflow.com/a/41213165/18121288
     bool readf::getconchar(KEY_EVENT_RECORD& krec)
     {
@@ -45,7 +68,7 @@ namespace console
     }
 
     // https://stackoverflow.com/a/18703309/18121288
-    // Helper function to get the state of modifier keys
+    // helper function to get the state of modifier keys
     DWORD readf::get_modifier_state(KEY_EVENT_RECORD& krec)
     {
         DWORD state = 0;
@@ -65,5 +88,48 @@ namespace console
             state = SHIFT_PRESSED;
 
         return state;
+    }
+
+    // (token_idx, char_idx)
+    std::pair<int, int> readf::get_token_diff(const std::string& text, const std::string& text2)
+    {
+        // tokenize the updated input text
+        this->lexer = lex(text);
+
+        // tokenize the updated rendered input text
+        lex ren_lexer = lex(text2);
+
+        int smallest_token_list_len = std::min(this->lexer.tokens.size(), ren_lexer.tokens.size());
+
+        for (int token_idx = 0; token_idx < smallest_token_list_len; token_idx++)
+        {
+            std::string text_token_name = this->lexer.tokens[token_idx].name;
+            std::string text2_token_name = ren_lexer.tokens[token_idx].name;
+
+            lex::token_type text_token_type = this->lexer.tokens[token_idx].type;
+            lex::token_type text2_token_type = ren_lexer.tokens[token_idx].type;
+
+            if (text_token_type != text2_token_type)
+                return {token_idx, 0};
+
+            else if (text_token_name != text2_token_name)
+                return {token_idx, this->get_text_diff(text_token_name, text2_token_name)};
+        }
+
+        return {smallest_token_list_len - 1, 0};
+    }
+
+    int readf::get_text_diff(const std::string& text, const std::string& text2)
+    {
+        for (int char_idx = 0; char_idx < std::min(text.length(), text2.length()); char_idx++)
+        {
+            if (text[char_idx] != text2[char_idx])
+                return char_idx;
+        }
+
+        if (text.length() != text2.length())
+            return std::min(text.length(), text2.length());
+
+        return 0;
     }
 }
--- src/core/readf/readf.h
+++ src/core/readf/readf.h
@@ -12,13 +12,13 @@ namespace console
         std::vector<lex::token> takeinput();
 
     private:
-        struct cursor_config
+        struct cursor_vec3
         {
             int x;
             int y;
             int i;
 
-            cursor_config(int x, int y, int i=0)
+            cursor_vec3(int x, int y, int i=0)
             {
                 this->x = x;
                 this->y = y;
@@ -26,25 +26,35 @@ namespace console
             }
         };
 
+        COORD init_cursor_pos;
         int suggestion_idx;
         std::vector<std::string> suggestions_list;
         std::string text_buffer;
         std::string ren_text_buffer; // rendered text buffer
-        readf::cursor_config config = readf::cursor_config(0, 0, 0);
+        readf::cursor_vec3 vector3 = readf::cursor_vec3(0, 0, 0);
         std::map<lex::token_type, console::color> color_codes = {};
         std::map<std::pair<WORD, DWORD>, std::function<void()>> key_codes = {};
         lex lexer = lex("");
+        std::pair<int, int> diff_token_idx;
 
     private:
-        bool getconchar(KEY_EVENT_RECORD& krec);
+        // define helper functions
         COORD get_cursor_pos();
         void set_cursor_pos(const COORD& c);
+        COORD calc_xy_coord(const int& total_dist);
+        int console_window_width();
+        int console_window_height();
+        bool getconchar(KEY_EVENT_RECORD& krec);
         DWORD get_modifier_state(KEY_EVENT_RECORD& krec);
+        void set_cursor_position(const int& total_dist);
+        std::pair<int, int> get_token_diff(const std::string& text, const std::string& text2);
+        int get_text_diff(const std::string& text, const std::string& text2);
 
         // define rendering functions
         void clear_console();
-        void update_console();
+        void update_console(const bool& render_suggestions=true);
         void render_tokens();
+        void render_token(const int& token_idx, const int& char_idx);
 
         // define key functions
         void handle_ctrl_enter();
