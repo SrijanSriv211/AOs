233e38dd29c15464355ce7037170c63086d0731d
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-17 18:57:41 +0530

    Create datetime.cpp & datetime.h

---
+++ src/shared/datetime/datetime.cpp
@@ -0,0 +1,16 @@
+#include "aospch.h"
+#include "datetime.h"
+
+namespace datetime
+{
+    std::string datetime()
+    {
+        const auto now = std::chrono::system_clock::now();
+        const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+
+        std::tm now_tm = *std::localtime(&t_c);
+        std::stringstream ss;
+        ss << std::put_time(&now_tm, "%d-%m-%Y %H:%M:%S");
+        return ss.str();
+    }
+}
---
+++ src/shared/datetime/datetime.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace datetime
+{
+    std::string datetime();
+}
