4aa64acc03a9f135c84bf178c5449a91aaf20341
SrijanSriv211 <QCoreNest@gmail.com>
2024-12-13 19:51:08 +0530

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
-#include "aospch.h"
+#include "aopch.h"
 #include "core/entrypoint/entrypoint.h"
 
 int main(int argc, char const *argv[])
 {
     std::vector<std::string> args(argv, argv + argc);
     args.erase(args.begin());
-    return take_entry(args);
+    int return_code = 0;
+
+    while (true)
+    {
+        return_code = take_entry(args);
+        if (return_code == 0)
+            break;
+
+        else if (return_code == -1)
+            return 1;
+    }
+
+    return return_code;
 }
