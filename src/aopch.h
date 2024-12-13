b10c1b9926f396359220822acfcfe4730c61ebd9
SrijanSriv211 <QCoreNest@gmail.com>
2024-12-13 19:46:14 +0530

    Create aopch.h

---
+++ src/aopch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <functional>
+#include <algorithm>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include <vector>
+#include <cctype>
+#include <format>
+#include <string>
+#include <chrono>
+#include <cmath>
+#include <regex>
+#include <map>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
