63639746201ff26dc52e506acfdca831c20d2096
SrijanSriv211 <QCoreNest@gmail.com>
2024-12-13 19:49:41 +0530

    Update startup.cpp

--- src/core/entrypoint/startup.cpp
+++ src/core/entrypoint/startup.cpp
@@ -1,11 +1,21 @@
-#include "aospch.h"
+#include "aopch.h"
 #include "entrypoint.h"
+#include "ao.h"
 
 #include "argparse/argparse.h"
 #include "console/console.h"
+#include "strings/strings.h"
+#include "array/array.h"
+
+#include "core/lexer/lex.h"
+#include "core/server/server.h"
+#include "core/execute/execute.h"
 
 std::vector<std::string> load_file(const std::string& filename)
 {
+    if (strings::is_empty(filename))
+        return {""};
+
     std::fstream file;
     std::string current_line;
     std::vector<std::string> code;
@@ -13,13 +23,34 @@ std::vector<std::string> load_file(const std::string& filename)
     file.open(filename);
 
     if (!file)
-        console::throw_error(filename + ": No such file or directory.", "File system io");
+        console::errors::throw_error(filename + ": No such file or directory.", "File system io");
 
     while (std::getline(file, current_line))
         code.push_back(current_line);
 
     file.close();
     return code;
+}
+
+void run_ao_script(const std::string& filename)
+{
+    std::vector<std::string> code = load_file(filename);
+
+    for (std::vector<std::string>::size_type i = 0; i < code.size(); i++)
+    {
+        lex lexer(code[i]);
+        execute_tokens(lexer.tokens);
+    }
+}
+
+void run_ao_scripts(const std::vector<std::string>& filenames)
+{
+    for (const std::string& filename : array::reduce(filenames))
+    {
+        console::print("> ", console::color::GRAY, false);
+        console::print(filename, console::color::LIGHT_WHITE);
+        run_ao_script(filename);
+    }
 }
 
 int exec_parsed_args(argparse& parser, const std::vector<argparse::parsed_argument>& parsed_args)
@@ -35,21 +66,20 @@ int exec_parsed_args(argparse& parser, const std::vector<argparse::parsed_argume
         else if (std::find(arg.names.begin(), arg.names.end(), "--init") != arg.names.end())
             init_folders();
 
-        else if (arg.names.front().ends_with(".aos"))
+        else if (std::find(arg.names.begin(), arg.names.end(), "--api") != arg.names.end())
         {
-            std::vector<std::string> code = load_file(arg.names.front());
-
-            for (int i = 0; i < code.size(); i++)
-            {
-                std::string* line = new std::string(code[i]);
-                exec_code(line);
-                delete line;
-            }
+            setup(); // show a setup screen with some basic details on first boot
+            AO::clear_console();
+            return start_server("127.0.0.1", 8000);
+            // curl -X POST http://127.0.0.1:8000 -d "shout Hello world!"
         }
+
+        else if (arg.names.front().ends_with(".ao"))
+            run_ao_script(arg.names.front());
 
         else
         {
-            unrecognized_argument_error(arg.names.front() + ": Please type 'aos --help' for more information");
+            unrecognized_argument_error(arg.names.front() + ": Please type 'ao --help' for more information");
             return 1;
         }
     }
